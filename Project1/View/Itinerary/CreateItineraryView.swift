511b29db9419ba143e431162ae722d797854d005
Dave Gumba <davegumba@gmail.com>
2023-09-14 19:53:02 -0400

    grouping stuff into scrollviews because of that 10 limit group rule

--- Project1/View/Itinerary/CreateItineraryView.swift
+++ Project1/View/Itinerary/CreateItineraryView.swift
@@ -15,6 +15,18 @@ struct CreateItineraryView: View {
     @State var city = ""
     @State var numberOfDays = 3
     
+    // FIXME: The viewmodel should have this
+    @State var interests = [InterestButton(interestTitle: "Alcohol"),
+                            InterestButton(interestTitle: "Coffee"),
+                            InterestButton(interestTitle: "Night Life"),
+                            InterestButton(interestTitle: "Nature"),
+                            InterestButton(interestTitle: "Good Weather"),
+                            InterestButton(interestTitle: "Culture"),
+                            InterestButton(interestTitle: "Beaches"),
+                            InterestButton(interestTitle: "Scenic spots"),
+                            InterestButton(interestTitle: "History"),
+                            InterestButton(interestTitle: "Good food")]
+    
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
@@ -42,64 +54,67 @@ struct CreateItineraryView: View {
                            
                             // Generate button
                             Button {
-                                Task { try await chatViewModel.sendItineraryRequest(city: city, numberOfDays: numberOfDays) }
                                 generatingItinerary.toggle()
+                                Task { try await chatViewModel.sendItineraryRequest(city: city, numberOfDays: numberOfDays) }
                             } label: {
                                 Text("Generate!")
                             }
                             .buttonStyle(CustomButton())
                             
-                        }
+                        } // group
                     }
                     .padding()
                 } else {
                     // Ideally this is its own view, GeneratedItineraryView
                     if !chatViewModel.isLoading {
                         // Duplicated from ItineraryDetailView
-                        ScrollView {
-                            LazyVStack {
-                                Text("\(numberOfDays) Days in \(city)")
-                                    .font(.largeTitle)
-                                
-                                Divider()
-                                
-                                ForEach(Array(chatViewModel.activities.enumerated()), id: \.offset) { index, element in
-                                    Text("Day \(index + 1)")
-                                        .font(.title)
-
-                                    ForEach(element, id: \.self) { activity in
-                                        VStack {
-                                            Text("\u{2022} \(activity)")
-                                                .padding(5)
-                                        }
-                                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading) // Achieves the Text() alignment we want
-                                    }
+                        Group {
+                            ScrollView {
+                                LazyVStack {
+                                    Text("\(numberOfDays) Days in \(city)")
+                                        .font(.largeTitle)
                                     
                                     Divider()
-                                }
-                                .padding(10)
-                            }
-                            
-                            if !chatViewModel.activities.isEmpty {
-                                HStack {
-                                    Button {
-                                        Task { try await createItineraryViewModel.uploadItinerary(city: self.city, numberOfDays: self.numberOfDays, details: chatViewModel.response) }
-                                        dismiss()
-                                    } label: {
-                                        Text("Save to my itineraries")
-                                    }
-                                    .buttonStyle(CustomButton())
                                     
-                                    Button {
-                                        generatingItinerary.toggle()
-                                    } label: {
-                                        Text("Generate another itinerary")
+                                    ForEach(Array(chatViewModel.activities.enumerated()), id: \.offset) { index, element in
+                                        Text("Day \(index + 1)")
+                                            .font(.title)
+
+                                        ForEach(element, id: \.self) { activity in
+                                            VStack {
+                                                Text("\u{2022} \(activity)")
+                                                    .padding(5)
+                                            }
+                                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading) // Achieves the Text() alignment we want
+                                        }
+                                        
+                                        Divider()
                                     }
-                                    .buttonStyle(CustomButton())
+                                    .padding(10)
                                 }
-                                .padding()
-                            }
-                        } // scrollview
+                                
+                                if !chatViewModel.activities.isEmpty {
+                                    HStack {
+                                        Button {
+                                            Task { try await createItineraryViewModel.uploadItinerary(city: self.city, numberOfDays: self.numberOfDays, details: chatViewModel.response) }
+                                            dismiss()
+                                        } label: {
+                                            Text("Save to my itineraries")
+                                        }
+                                        .buttonStyle(CustomButton())
+                                        
+                                        Button {
+                                            generatingItinerary.toggle()
+                                        } label: {
+                                            Text("Generate another itinerary")
+                                        }
+                                        .buttonStyle(CustomButton())
+                                    }
+                                    .padding()
+                                }
+                            } // scrollview
+                        }
+                        
                         .padding()
                     } else {
                         Spacer()
